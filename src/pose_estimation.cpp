d9c278fe301b688aff4de4471120b97f039bb5ae
AlvinNg727 <alvinng2707@gmail.com>
2026-07-19 17:50:32 +0800

    feat: add bundle_pnp for multi-tag bundle PnP estimation

--- src/pose_estimation.cpp
+++ src/pose_estimation.cpp
@@ -54,6 +54,47 @@ pnp(apriltag_detection_t* const detection, const std::array<double, 4>& intr, do
     return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
 }
 
+geometry_msgs::msg::Transform
+bundle_pnp(
+    const std::vector<apriltag_detection_t*>& detections,
+    const std::array<double, 4>& intr,
+    const std::unordered_map<int, std::array<double, 3>>& bundle_tag_positions,
+    const std::unordered_map<int, double>& tag_sizes,
+    double default_size)
+{
+    std::vector<cv::Point3d> objectPoints;
+    std::vector<cv::Point2d> imagePoints;
+
+    for(const auto& det : detections) {
+        auto it = bundle_tag_positions.find(det->id);
+        if(it == bundle_tag_positions.end()) continue;
+
+        const auto& pos = it->second;
+        const double size = tag_sizes.count(det->id) ? tag_sizes.at(det->id) : default_size;
+        const double hs = size / 2.0;
+
+        objectPoints.emplace_back(pos[0] - hs, pos[1] - hs, pos[2]);
+        objectPoints.emplace_back(pos[0] + hs, pos[1] - hs, pos[2]);
+        objectPoints.emplace_back(pos[0] + hs, pos[1] + hs, pos[2]);
+        objectPoints.emplace_back(pos[0] - hs, pos[1] + hs, pos[2]);
+
+        for(int i = 0; i < 4; i++) {
+            imagePoints.emplace_back(det->p[i][0], det->p[i][1]);
+        }
+    }
+
+    cv::Matx33d cameraMatrix;
+    cameraMatrix(0, 0) = intr[0];
+    cameraMatrix(1, 1) = intr[1];
+    cameraMatrix(0, 2) = intr[2];
+    cameraMatrix(1, 2) = intr[3];
+
+    cv::Mat rvec, tvec;
+    cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
+
+    return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+}
+
 const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods{
     {"homography", homography},
     {"pnp", pnp},
--- src/pose_estimation.hpp
+++ src/pose_estimation.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <apriltag/apriltag.h>
+#include <array>
 #include <functional>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <unordered_map>
+#include <vector>
 
 
 typedef std::function<geometry_msgs::msg::Transform(apriltag_detection_t* const, const std::array<double, 4>&, const double&)> pose_estimation_f;
 
 extern const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods;
+
+geometry_msgs::msg::Transform bundle_pnp(
+    const std::vector<apriltag_detection_t*>& detections,
+    const std::array<double, 4>& intrinsics,
+    const std::unordered_map<int, std::array<double, 3>>& bundle_tag_positions,
+    const std::unordered_map<int, double>& tag_sizes,
+    double default_size);
