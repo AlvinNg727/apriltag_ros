57b88fcf57df0cd23e99ce63c63da931ae429db5
AlvinNg727 <alvinng2707@gmail.com>
2026-07-19 18:03:37 +0800

    feat: add apriltag_mono launch file for gz simulation setup

---
+++ launch/apriltag_mono.launch.py
@@ -0,0 +1,32 @@
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="apriltag_ros",
+                executable="apriltag_node",
+                name="apriltag",
+                namespace="apriltag",
+                output="screen",
+                remappings=[
+                    ("image_rect", "/mono_cam_down/image"),
+                    ("camera_info", "/mono_cam_down/camera_info"),
+                ],
+                parameters=[
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("apriltag_ros"),
+                            "cfg",
+                            "tags_36h11.yaml",
+                        ]
+                    ),
+                ],
+            ),
+        ]
+    )
