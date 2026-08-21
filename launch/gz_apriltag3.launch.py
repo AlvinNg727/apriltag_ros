6b0f07b976b770311dd9fae05c77911e0a458143
AlvinNg727 <alvinng2707@gmail.com>
2026-08-21 20:45:33 +0800

    feat: add apriltag3 configuration and launch files for simulation setup

---
+++ launch/gz_apriltag3.launch.py
@@ -0,0 +1,33 @@
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="apriltag_ros",
+                executable="apriltag_node",
+                name="apriltag",
+                namespace="apriltag",
+                output="screen",
+                remappings=[
+                    ("image_rect", "/mono_cam_down/image"),
+                    ("camera_info", "/mono_cam_down/camera_info"),
+                ],
+                parameters=[
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("apriltag_ros"),
+                            "cfg",
+                            "apriltag3.yaml",
+                        ]
+                    ),
+                    {"use_sim_time": True},
+                ],
+            ),
+        ]
+    )
