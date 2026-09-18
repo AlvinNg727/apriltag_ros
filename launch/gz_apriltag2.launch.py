1c97b2c0f03bf272604b04ae18c48710b9fae2ae
AlvinNg727 <alvinng2707@gmail.com>
2026-09-18 17:46:12 +0800

    feat: update YAML and launch files for consistent formatting and structure

--- launch/gz_apriltag2.launch.py
+++ launch/gz_apriltag2.launch.py
@@ -1,8 +1,7 @@
+from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-from launch import LaunchDescription
 
 
 def generate_launch_description():
